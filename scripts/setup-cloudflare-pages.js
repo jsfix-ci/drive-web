6beb093f536b6b852579fdf632b8a93600d31c61
Alex Menor <alex.menor@icloud.com>
2021-12-21 16:03:57 +0100

    debugging cloudflare env

--- scripts/setup-cloudflare-pages.js
+++ scripts/setup-cloudflare-pages.js
@@ -7,3 +7,5 @@ const fileContent = fs.readFileSync('.npmrc.template', { encoding: 'utf-8' });
 const fileToWrite = fileContent.replace('${NPM_TOKEN}', process.env.NPM_TOKEN);
 
 fs.writeFileSync('.npmrc', fileToWrite);
+
+console.log(fs.readFileSync('.npmrc', { encoding: 'utf-8' }));
