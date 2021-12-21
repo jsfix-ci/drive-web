863dd891a4d59dd1eba4e5fff52f3c47a818ee4c
Alex Menor <alex.menor@icloud.com>
2021-12-21 15:58:35 +0100

    replace token manually in .npmrc file

--- scripts/setup-cloudflare-pages.js
+++ scripts/setup-cloudflare-pages.js
@@ -2,4 +2,8 @@ const fs = require('fs');
 
 if (!process.env.CF_PAGES) process.exit(0);
 
-fs.copyFileSync('.npmrc.template', '.npmrc');
+const fileContent = fs.readFileSync('.npmrc.template', { encoding: 'utf-8' });
+
+const fileToWrite = fileContent.replace('${NPM_TOKEN}', process.env.NPM_TOKEN);
+
+fs.writeFileSync('.npmrc', fileToWrite);
